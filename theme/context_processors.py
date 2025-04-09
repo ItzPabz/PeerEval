dfd6194134f5716f870cdd75e0b18b65c517866a
Pabz <64668341+ItzPabz@users.noreply.github.com>
2025-04-09 10:35:22 -0400

    Remove unused variable INST_COLOR

--- theme/context_processors.py
+++ theme/context_processors.py
@@ -7,5 +7,4 @@ def institution_settings(request):
     return {
         'INST_NAME': settings.INST_NAME,
         'INST_SHORT_NAME': settings.INST_SHORT_NAME,
-        'INST_COLOR': settings.INST_COLOR,
     } 
