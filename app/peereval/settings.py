290945b3f9c015b99909a4edd4d024f22e530ac6
Pabz <64668341+ItzPabz@users.noreply.github.com>
2025-05-09 23:42:16 -0400

    Using environ

--- app/peereval/settings.py
+++ app/peereval/settings.py
@@ -114,11 +114,11 @@ WSGI_APPLICATION = 'peereval.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
